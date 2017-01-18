7af435c7b98dfa6f00f876f3d721ce8cf3172c50
xuzhen <964190389@qq.com>
2017-01-18 11:24:49 +0800

    创建

---
+++ AZDocInteraction/AZDocInteraction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AZDocInteraction
+//
+//  Created by liguzhun on 2017/1/18.
+//  Copyright © 2017年 XuZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AZDocInteraction/AZDocInteraction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AZDocInteraction
+//
+//  Created by liguzhun on 2017/1/18.
+//  Copyright © 2017年 XuZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
